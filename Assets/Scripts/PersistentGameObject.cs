619f8a140a0b3db80e0d3c108697820d5f004865
Mike Miller <mmikecha@gmail.com>
2018-05-13 15:39:54 -0400

    Working on menus. Persistent scene manager and network manager. can go from main menu to create game menu

---
+++ Assets/Scripts/PersistentGameObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PersistentGameObject : MonoBehaviour
+{
+
+	private static bool _created = false;
+
+	void Awake() {
+		if (!_created) {
+			DontDestroyOnLoad(this.gameObject);
+			_created = true;
+		}
+	}
+}
